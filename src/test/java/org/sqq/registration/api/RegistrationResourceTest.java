52d84ef319dc4392447262fac471157006b97d27
Lucas Declercq <lucas.declercq@hoohoot.org>
2025-12-10 09:08:23 +0100

    test: add integration test

---
+++ src/test/java/org/sqq/registration/api/RegistrationResourceTest.java
@@ -0,0 +1,50 @@
+package org.sqq.registration.api;
+
+import com.stripe.exception.StripeException;
+import io.quarkus.test.junit.QuarkusMock;
+import io.quarkus.test.junit.QuarkusTest;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
+import org.sqq.registration.stripe.Stripe;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import static io.restassured.RestAssured.given;
+
+@QuarkusTest
+class RegistrationResourceTest {
+    
+    @Inject
+    Stripe stripe;
+
+    @BeforeAll
+    public static void setup() throws StripeException, URISyntaxException {
+        Stripe mockedStripe = Mockito.mock(Stripe.class);
+        Mockito.when(mockedStripe.paySouscription(ArgumentMatchers.any())).thenReturn(new URI("https://checkout.stripe.com"));
+        QuarkusMock.installMockForType(mockedStripe, Stripe.class);
+    }
+    
+    @Test
+    void shouldCreateCooperateur() {
+        given()
+                .formParam("genre", "MADAME")
+                .formParam("prenom", "Jean")
+                .formParam("nom", "Michel")
+                .formParam("telephone", "0600000000")
+                .formParam("email", "jean.michel@mailbox.org")
+                .formParam("adresse", "184 rue du marais")
+                .formParam("ville", "Lille")
+                .formParam("codePostal", "59000")
+                .formParam("etudiantOuMinimasSociaux", "false")
+                .formParam("nombreDePersonnesDansLeFoyer", "2")
+                .formParam("partsDeSoutien", "0")
+                .formParam("acceptationDesStatus", "true")
+                .contentType("application/x-www-form-urlencoded")
+                .when().post("/api/v1/registrations")
+                .then().statusCode(303);
+    }
+}
