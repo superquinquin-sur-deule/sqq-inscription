6394a1f36ec6ccf4e0716e97acbcabb54b873510
Lucas Declercq <lucas.declercq@hoohoot.org>
2025-12-15 08:42:37 +0100

    feat: add plugin to load umami script programmatically

--- src/main/webui/vite.config.ts
+++ src/main/webui/vite.config.ts
@@ -1,10 +1,20 @@
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import VitePluginUmami from "./src/utils/umami";
 
 // https://vite.dev/config/
 export default defineConfig({
     base: '',
-    plugins: [vue()],
+    plugins: [
+        vue(),
+        VitePluginUmami({
+            enableDev: false,
+            umami: {
+                id: "28ad4313-919a-4fe1-a6ba-ef7b7442e7b4",
+                src: "https://umami.hoohoot.org/script.js"
+            }
+        })
+    ],
     server: {
         port: 3000,
     },
